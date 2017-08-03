3f2709f62f792ceeb19bbbe11f8c6610439a266f
Alex Lupu <ext-alex.lupu@trimble.com>
2017-08-03 08:03:16 +0300

    Add model object get and select shortcuts, and get and set uda

---
+++ Library/Model/GetObject.cs
@@ -0,0 +1,33 @@
+﻿using TSM = Tekla.Structures.Model;
+using TS = Tekla.Structures;
+
+namespace Tekla.Structures.Model
+{
+    public static class GetObject
+    {
+        /// <summary>
+        /// Gets a model object by Guid
+        /// </summary>
+        /// <param name="Guid">Tekla object GUID</param>
+        /// <returns>ModelObject or null if invalid id</returns>
+        public static TSM.ModelObject Get(string Guid) =>
+            Get(new TSM.Model().GetIdentifierByGUID(Guid));
+
+        /// <summary>
+        /// Gets a model object by int ID
+        /// </summary>
+        /// <param name="Id">Tekla object int ID</param>
+        /// <returns>ModelObject or null if invalid id</returns>
+        public static TSM.ModelObject Get(int Id) =>
+            Get(new TS.Identifier(Id));
+
+        /// <summary>
+        /// Gets a model object by tekla identifier
+        /// </summary>
+        /// <param name="Identifier">Tekla object identifier</param>
+        /// <returns>ModelObject or null if invalid identifier</returns>
+        public static TSM.ModelObject Get(TS.Identifier Identifier) =>
+            new TSM.Model().SelectModelObject(Identifier);
+
+    }
+}
---
+++ Library/Model/GetUDA.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using TSM = Tekla.Structures.Model;
+using TS = Tekla.Structures;
+
+namespace Tekla.Structures.Model
+{
+    public static partial class UDA
+    {
+        public static object GetUDA(string Guid, string Uda)
+            => GetUDA(GetObject.Get(Guid), Uda);
+
+        public static object GetUDA(int Id, string Uda)
+            => GetUDA(GetObject.Get(Id), Uda);
+
+        public static object GetUDA(TS.Identifier Identifier, string Uda)
+            => GetUDA(GetObject.Get(Identifier), Uda);
+
+        public static object GetUDA(TSM.ModelObject ModelObj, string Uda)
+        {
+            string refString = "";
+
+            ModelObj.GetUserProperty(Uda, ref refString);
+
+            if(refString.Length < 1)
+            {
+                double refDouble = double.NaN;
+
+                ModelObj.GetUserProperty(Uda, ref refDouble);
+
+                if(double.IsNaN(refDouble))
+                {
+                    int refInt = int.MinValue;
+
+                    ModelObj.GetUserProperty(Uda, ref refInt);
+
+                    if(refInt != int.MinValue)
+                        return refInt;
+
+                    return null;
+                }
+                else
+                {
+                    return refDouble;
+                }
+            }
+            else
+            {
+                return refString;
+            }
+        }
+
+        public static Dictionary<string, object> GetUDAs(string Guid, List<string> Udas)
+            => GetUDAs(GetObject.Get(Guid), Udas);
+
+        public static Dictionary<string, object> GetUDAs(int Id, List<string> Udas)
+            => GetUDAs(GetObject.Get(Id), Udas);
+
+        public static Dictionary<string, object> GetUDAs(TS.Identifier Identifier, List<string> Udas)
+            => GetUDAs(GetObject.Get(Identifier), Udas);
+
+        public static Dictionary<string, object> GetUDAs(TSM.ModelObject ModelObj, List<string> Udas)
+        {
+            Dictionary<string, object> Items = new Dictionary<string, object>();
+
+            foreach(var item in Udas)
+                Items.Add(item, GetUDA(ModelObj, item));
+
+            return Items;
+        }
+    }
+}
